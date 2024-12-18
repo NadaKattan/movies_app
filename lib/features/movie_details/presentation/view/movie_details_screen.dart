fb05df2e0f3641890711099cb35e4cad8a7d2b95
NadaKattan <nadakattan012@gmail.com>
2024-12-18 23:21:33 +0200

    edit screens

--- lib/features/movie_details/presentation/view/movie_details_screen.dart
+++ lib/features/movie_details/presentation/view/movie_details_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:movies/core/routes/routes.dart';
+import 'package:movies/features/movie_details/presentation/view/widgets/movie_details_view.dart';
 
-class MovieDetailsScreen extends StatelessWidget {
+
+class MovieDetailsScreen extends StatefulWidget {
   const MovieDetailsScreen({super.key});
 
   @override
+  State<MovieDetailsScreen> createState() => _MovieDetailsScreenState();
+}
+
+class _MovieDetailsScreenState extends State<MovieDetailsScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final args = ModalRoute.of(context)!.settings.arguments as String?;
+    //  ??"575264";
+    return Scaffold(
+      appBar: AppBar(
+        // automaticallyImplyLeading: false,
+        title: const Text(
+          "Movies App",
+          style: TextStyle(fontSize: 20),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(Routes.watchList);
+              },
+              icon: const Icon(Icons.watch))
+        ],
+      ),
+      body: MovieDetailsView(args ?? "575264"),
+    );
   }
 }
--- lib/features/watch_list/presentation/view/watch_list_screen.dart
+++ lib/features/watch_list/presentation/view/watch_list_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:movies/features/watch_list/presentation/view/widgets/watch_list_tab.dart';
+
 
 class WatchListScreen extends StatelessWidget {
   const WatchListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: const WatchListTab(),
+    );
   }
 }
